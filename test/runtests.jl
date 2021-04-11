e2eaa0cd50cff14beb171cfef7245474f442861c
Tomas Omasta <omastto1@fel.cvut.cz>
2021-04-11 19:19:09 +0200

    Revert "Changed reference solver (SARSOP -> POMDPSolve)"
    This reverts commit 978f5492548b232df302ef7fadc6561856192141.

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,20 +1,20 @@
 using Test
 using POMDPModels
 using POMDPs
-using POMDPSolve
+using SARSOP
 using BeliefUpdaters
 
 using PointBasedValueIteration
 
-@testset "Comparison with POMDPSolve" begin
+@testset "Comparison with SARSOP" begin
     pomdps = [TigerPOMDP(), BabyPOMDP(), MiniHallway()]
 
     for pomdp in pomdps
         solver = PBVISolver()
         policy = solve(solver, pomdp)
 
-        pomdpsolver = POMDPSolveSolver(verbose=:False)
-        pomdpsolver_policy = solve(pomdpsolver, pomdp)
+        sarsop = SARSOPSolver(verbose=false)
+        sarsop_policy = solve(sarsop, pomdp)
 
         B = []
         if typeof(pomdp) == MiniHallway
@@ -35,7 +35,14 @@ using PointBasedValueIteration
         end
 
         pbvi_vals = [value(policy, b) for b in B]
-        pomdpsolver_vals = [value(pomdpsolver_policy, b) for b in B]
-        @test isapprox(pomdpsolver_vals, pbvi_vals, rtol=0.1)
+        if typeof(pomdp) == MiniHallway
+            sarsop_vals = [value(sarsop_policy, b) for b in B]
+            # Test passes when the value function is multiplied
+            # @test isapprox(sarsop_vals * 3.4 , pbvi_vals, rtol=0.3)
+            @test_broken isapprox(sarsop_vals, pbvi_vals, rtol=0.3)
+        else
+            sarsop_vals = [value(sarsop_policy, b) for b in B]
+            @test isapprox(sarsop_vals, pbvi_vals, rtol=0.1)
+        end
     end
 end
